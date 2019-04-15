34e5cefad7562195085f5e4a84991ac97de1b82e
root <root@localhost.localdomain>
2019-04-15 17:42:40 +0000

    Added basic timing for john

--- scripts/john_the_ripper_test.sh
+++ scripts/john_the_ripper_test.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+cd ~/CMPE132Kali/password_results
+
 if [[ $1 == "clearpot" ]]; then
 	rm ~/.john/john.pot
 fi
 
-cd ~/CMPE132Kali/password_results
+if ! [ -f time.txt ]; then
+	touch time.txt
+fi
+
+
 unshadow /etc/passwd /etc/shadow > mypasswd
-john mypasswd
+{ echo -e "\n\nTime for default parameters" ; } >>time.txt
+{ time john mypasswd 2> john.stderr; } 2>> time.txt
 echo -e "\nJohn the Ripper Output:"
 john mypasswd --show
 # cat ~/.john/john.pot
