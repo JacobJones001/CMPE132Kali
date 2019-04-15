f6d6cf7fbc72e8a8f377277f79792c15e569eb73
root <root@localhost.localdomain>
2019-04-15 17:11:33 +0000

    Added info about passwords cracked

--- scripts/john_the_ripper_test.sh
+++ scripts/john_the_ripper_test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [[ $1 == "clearpot" ]]; then
+	rm ~/.john/john.pot
+fi
+
 cd ~/CMPE132Kali/password_results
 unshadow /etc/passwd /etc/shadow > mypasswd
 john mypasswd
+echo -e "\nJohn the Ripper Output:"
+john mypasswd --show
+# cat ~/.john/john.pot
