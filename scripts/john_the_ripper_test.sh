f7ad875686974f22dacf8b972fabf10e841b5aaa
root <root@localhost.localdomain>
2019-05-13 05:40:16 +0000

    Added sha512crypt crack

--- scripts/john_the_ripper_test.sh
+++ scripts/john_the_ripper_test.sh
@@ -6,8 +6,7 @@ if ! [ -f time.txt ]; then
 	touch time.txt
 fi
 
-TEST_JOHN_MODE=default
-TEST_JOHN_OPTIONS= 
+
 
 unshadow /etc/passwd /etc/shadow > mypasswd
 { echo -e "password, mode, time" ; } > time.csv # csv header
@@ -21,11 +20,27 @@ for ((i=1;i<=END;i++)); do
 	fi
 	{ time john $TEST_JOHN_OPTIONS mypasswd 2> john.stderr; } 2> time_temp.txt
 	TEST_JOHN_TIME=$(cat time_temp.txt) 	# may need to clean up number to work in graph
-	TEST_JOHN_PASS=$(john --show mypasswd  | grep -oP '(?<=root:).*(?=:0:0)') # specific to root
-	echo $TEST_JOHN_PASS, default, $TEST_JOHN_TIME >> time.csv
+	
+	if [[ $TEST_JOHN_MODE == "descrypt" ]]; then
+		TEST_JOHN_PASS=$(john --show mypasswd  | grep -oP '(?<=root:).*(?=:0:0)') # specific to root
+	fi
+	if [[ $TEST_JOHN_MODE == "sha512crypt" ]]; then
+		TEST_JOHN_PASS=$(john --show mypasswd  | grep -oP '(?<=Harman:).*(?=:998:998)') # specific to root
+	fi
+	echo $TEST_JOHN_PASS, $TEST_JOHN_MODE, $TEST_JOHN_TIME >> time.csv
 done
 }
-TEST_RUN_JOHN 2 $1
+TEST_JOHN_MODE=descrypt
+TEST_JOHN_OPTIONS=--format=descrypt
+TEST_RUN_JOHN $1 $2
+
+TEST_JOHN_MODE=sha512crypt
+TEST_JOHN_OPTIONS=--format=sha512crypt
+TEST_RUN_JOHN $1 $2
+
+TEST_JOHN_MODE=descrypt
+TEST_JOHN_OPTIONS=--format=descrypt
+TEST_RUN_JOHN 1
 
 echo -e "\nJohn the Ripper Output:"
 john mypasswd --show
